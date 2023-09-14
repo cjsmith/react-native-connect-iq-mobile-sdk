6a629029d55c72b978911cd8108578ee64a2d1ad
Colin Smith <mail.colin.smith@gmail.com>
2023-09-14 10:31:01 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-connect-iq-mobile-sdk
+
+This package provides a React Native wrapper around the Android and iOS Garmin Connect IQ Mobile SDKs
+
+## Installation
+
+```sh
+npm install react-native-connect-iq-mobile-sdk
+```
+
+## Usage
+
+```js
+import { multiply } from 'react-native-connect-iq-mobile-sdk';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/connectiqmobilesdk/ConnectIqMobileSdkModule.java
@@ -0,0 +1,32 @@
+package com.connectiqmobilesdk;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = ConnectIqMobileSdkModule.NAME)
+public class ConnectIqMobileSdkModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "ConnectIqMobileSdk";
+
+  public ConnectIqMobileSdkModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
